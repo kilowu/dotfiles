fba94ccb5a0c37d7fb78d3fadac3e02f534584d2
Wu Wei <weiwu@cacheme.net>
2019-01-02 23:57:23 +0800

    Make tab completion on helm works in terminal

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -135,7 +135,7 @@
   :init (helm-mode 1)
   :config
   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-  ;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
   (define-key helm-map (kbd "C-z")  'helm-select-action)) ; list actions using C-z
 
 
